71fb2a8e8cf96d5de932e3b1f692cb8d6cb8a5ed
Lance Cassidy <lwcassid@gmail.com>
2026-08-12 09:56:17 -0400

    build.sh: add part37 (SG5)

--- tools/build.sh
+++ tools/build.sh
@@ -42,6 +42,7 @@ cd "$(dirname "$0")/.."
   cat parts/part34_fb12.js
   cat parts/part35_sg3.js
   cat parts/part36_sg4.js
+  cat parts/part37_sg5.js
   cat parts/part15_history.js
   cat parts/part5_tail.js
   printf '</script>\n</body>\n</html>\n'
